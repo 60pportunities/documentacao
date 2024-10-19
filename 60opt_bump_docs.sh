3b17cac70d3ad0a1571cbd7a8a5af8862f3b53c9
Horacio <horacio.vasconcellos@gmail.com>
2024-10-19 08:47:58 -0300

    <doc> Criação versão documentaca

--- 60opt_bump_docs.sh
+++ 60opt_bump_docs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #mike delete --all
-
+set -x
+git commit -m "<doc> Criação versão documentaca" --allow-empty
 LASTTAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-
 mike deploy --update-aliases "${LASTTAG}" latest
+mike set-default --push latest
+
 echo "PublicadaL: ${LASTTAG}"
